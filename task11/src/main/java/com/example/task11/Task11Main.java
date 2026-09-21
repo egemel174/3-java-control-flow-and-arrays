91390669a9328d8f4d1ccfedbf6036d12dc855aa
egemel <egemel174@gmail.com>
2026-09-21 13:11:24 +0500

    tasks all done

--- task11/src/main/java/com/example/task11/Task11Main.java
+++ task11/src/main/java/com/example/task11/Task11Main.java
@@ -4,15 +4,29 @@ public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        int min = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= arr[min]) {
+                min = i;
+            }
+        }
+
+        int temp = arr[0];
+        arr[0] = arr[min];
+        arr[min] = temp;
     }
 
 }
